c83fcb36ca3310f94cebe18f5967163c6bfee130
FuseTim <fusetim@gmx.com>
2024-03-06 11:24:17 +0100

    feat(server): listen on port

--- src/server.c
+++ src/server.c
@@ -2,12 +2,43 @@
 #include <stdlib.h>
 #include <stdbool.h>
 #include <string.h>
+#include <stdint.h>
 
 #ifndef _LIB_SDL_NET_H
 #define _LIB_SDL_NET_H
 #define WITHOUT_SDL
 #include <SDL2/SDL_net.h>
 #endif
+
+#ifndef _LIB_DOOM_NET_UTIL_H
+#include "../include/net/util.h"
+#endif
+
+int run_server(uint16_t port) {
+    printf("Initializing SDL_net...\n");
+    SDLNet_Init();
+
+    // Open socket
+    IPaddress ip;
+    TCPsocket server;
+    SDLNet_ResolveHost(&ip, NULL, port);
+    server = SDLNet_TCP_Open(&ip);
+    if (!server) {
+        printf("ERR SDLNet_TCP_Open: %s\n", SDLNet_GetError());
+        return -1;
+    }
+    char addrstr[24];
+    addrtocstr(&ip, addrstr);
+    printf("Listening on %s\n", addrstr);
+
+    
+
+    // Clean-up
+    printf("Shutting down...\n");
+    SDLNet_TCP_Close(server);
+    SDLNet_Quit();
+    return 0;
+}
 
 int main(int argc, char const *argv[])
 {
@@ -16,9 +47,9 @@ int main(int argc, char const *argv[])
         printf("Insufficient argument: missing port number.\nUsage: server <port>");
         return -1;
     }
-    int port = atoi(argv[1]);
-    printf("Listening on port %d...\n", port);
-
+    uint16_t port = atoi(argv[1]);
+    printf("Running server on port %d...\n", port);
+    run_server(port);
     printf("Exiting...\n");
     return 0;
 }
