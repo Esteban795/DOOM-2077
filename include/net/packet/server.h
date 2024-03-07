75313d76d98b313588e1be6e0420c450ab22180f
FuseTim <fusetim.log@gmx.com>
2024-03-07 19:09:33 +0100

    feat(net): beginning network protocol impl/spec

---
+++ include/net/packet/server.h
@@ -0,0 +1,13 @@
+#define _LIB_DOOM_NET_PACKET_SERVER_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
+#include <stdint.h>
+
+#ifndef _LIB_SDL_NET_H
+#define _LIB_SDL_NET_H
+#define WITHOUT_SDL
+#include <SDL2/SDL_net.h>
+#endif
---
+++ src/net/packet/server.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
+#include <stdint.h>
+
+#ifndef _LIB_DOOM_NET_PACKET_SERVER_H
+#include "../../../include/net/packet/server.h"
+#endif
+
+#ifndef _LIB_SDL_NET_H
+#define _LIB_SDL_NET_H
+#define WITHOUT_SDL
+#include <SDL2/SDL_net.h>
+#endif
