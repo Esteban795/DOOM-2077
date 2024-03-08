a99119885032cdbc3b9c62d3feb752a816ae1b67
FuseTim <fusetim@gmx.com>
2024-03-08 18:57:59 +0100

    fix: oops

--- src/net/util.c
+++ src/net/util.c
@@ -26,25 +26,25 @@ void write_uint8(uint8_t* buf, uint8_t data) {
 
 void write_uint16(uint8_t* buf, uint16_t data) {
     buf[0] = data >> 8;
-    buf[1] = data;
+    buf[1] = data | 0xFF;
 }
 
 void write_uint32(uint8_t* buf, uint32_t data) {
     buf[0] = data >> 24;
-    buf[1] = data >> 16;
-    buf[2] = data >> 8;
-    buf[3] = data;
+    buf[1] = data >> 16 | 0xFF;
+    buf[2] = data >> 8  | 0xFF;
+    buf[3] = data       | 0xFF;
 }
 
 void write_uint64(uint8_t* buf, uint64_t data) {
     buf[0] = data >> 56;
-    buf[1] = data >> 48;
-    buf[2] = data >> 40;
-    buf[3] = data >> 32;
-    buf[4] = data >> 24;
-    buf[5] = data >> 16;
-    buf[6] = data >> 8;
-    buf[7] = data;
+    buf[1] = data >> 48 | 0xFF;
+    buf[2] = data >> 40 | 0xFF;
+    buf[3] = data >> 32 | 0xFF;
+    buf[4] = data >> 24 | 0xFF;
+    buf[5] = data >> 16 | 0xFF;
+    buf[6] = data >> 8  | 0xFF;
+    buf[7] = data       | 0xFF;
 }
 
 int write_cstring(uint8_t* buf, char* data) {
@@ -73,13 +73,13 @@ uint32_t read_uint32(uint8_t* buf) {
 
 uint64_t read_uint64(uint8_t* buf) {
     uint64_t data = buf[7];
-    data |= (uint64_t) buf[6] << 8;
-    data |= (uint64_t) buf[5] << 16;
-    data |= (uint64_t) buf[4] << 24;
-    data |= (uint64_t) buf[3] << 32;
-    data |= (uint64_t) buf[2] << 40;
-    data |= (uint64_t) buf[1] << 48;
-    data |= (uint64_t) buf[0] << 56;
+    data |= ((uint64_t) buf[6]) << 8;
+    data |= ((uint64_t) buf[5]) << 16;
+    data |= ((uint64_t) buf[4]) << 24;
+    data |= ((uint64_t) buf[3]) << 32;
+    data |= ((uint64_t) buf[2]) << 40;
+    data |= ((uint64_t) buf[1]) << 48;
+    data |= ((uint64_t) buf[0]) << 56;
     return data;
 }
 
