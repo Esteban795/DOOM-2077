e5543541c2c87fb5d718367b5840855817b3b010
Esteban MANDJUKOWSKI <estebanmandjukowski@gmail.com>
2024-04-10 11:46:35 +0200

    fix : providing stdenv for right compiler

--- flake.nix
+++ flake.nix
@@ -16,7 +16,7 @@
        
 
       devShells = {
-          default = pkgs.mkShell.override { stdenv = pkgs.clangStdenv; } {
+          default = pkgs.mkShell.override { stdenv = pkgs.gccStdenv; } {
             packages = with pkgs; [
               boost
               catch2
