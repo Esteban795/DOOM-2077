4f70395902362259ab438eca08ad5710426f0919
Esteban MANDJUKOWSKI <estebanmandjukowski@gmail.com>
2024-04-22 16:03:48 +0200

    rework : starting reworking the door system

--- include/door.h
+++ include/door.h
@@ -5,9 +5,64 @@
 #include "ecs/entity.h"
 #include "sector.h"
 #include "geometry.h"
-
+#include "sidedef.h"
 
 #define MINIMUM_FLOOR_HEIGHT -512
+
+// By default, P1 / PR and W1 / WR will still be activated in deathmatch 
+// OWC = open wait close
+// OSO = open stay open
+// CSC = close stay closed
+// CWO = close wait open
+// S = slow
+// F = fast
+// SR = switchable, retriggerable
+// S1 = switchable, non-retriggerable
+// WR = walkover, retriggerable
+// W1 = walkover, non-retriggerable
+// GR = gun, retriggerable
+// G1 = gun, non-retriggerable
+// PR = pushed, retriggerable
+// P1 = pushed, non-retriggerable
+// DR = door with key
+enum DoorTypes {
+  CSC_SR_S = 42,
+  CSC_SR_F = 116,
+  CSC_S1_S = 50,
+  CSC_S1_F = 113,
+  CSC_WR_S = 75,
+  CSC_WR_F = 107,
+  CSC_W1_S = 3,
+  CSC_W1_F = 110,
+  CWO_SR_S = 196,
+  CWO_S1_S = 175,
+  CWO_WR_S = 76,
+  CWO_W1_S = 16,
+  OSO_P1_S = 31,
+  OSO_P1_F = 118,
+  OSO_SR_S = 61,
+  OSO_SR_F = 115,
+  OSO_S1_S = 103,
+  OSO_S1_F = 112,
+  OSO_WR_S = 86,
+  OSO_WR_F = 106,
+  OSO_W1_S = 2,
+  OSO_W1_F = 109,
+  OSO_GR_S = 46,
+  OWC_PR_S = 1,
+  OWC_PR_F = 117,
+  OWC_SR_S = 63,
+  OWC_SR_F = 114,
+  OWC_S1_S = 29,
+  OWC_S1_F = 111,
+  OWC_WR_S = 90,
+  OWC_WR_F = 105,
+  OWC_W1_S = 4,
+  OWC_W1_F = 108,
+  DR_RED_KEY = 28,
+  DR_YELLOW_KEY = 27,
+  DR_BLUE_KEY = 26,
+};
 
 enum DoorTransitionSpeed { // percentage of total height achieved per second
     NO_SPEED = 0,
@@ -32,6 +87,7 @@ struct Door {
     bool is_open;
     bool is_switching;
     bool is_colllidable;
+    bool is_repeatable;
     bool is_shootable;
     i16 delta_height;
     i16 max_height;
@@ -41,10 +97,6 @@ struct Door {
 typedef struct Door door;
 
 extern door* COLLISIONNED_DOOR;
-
-door *door_create(entity_t *id, enum DoorTransitionSpeed speed,
-                  enum DoorFunction function, int wait_time, bool is_collidable,bool is_shootable,
-                  sector *sector);
 
 void door_timeout(door *d, int DT);
 
@@ -57,4 +109,6 @@ void door_update_height(door *d, int delta_height);
 void door_trigger_switch(door *d);
 
 void doors_free(door** doors,int len_doors);
+
+door *create_door_from_linedef(sidedef *sd, enum DoorTypes line_type);
 #endif
--- src/door.c
+++ src/door.c
@@ -6,7 +6,7 @@ door *COLLISIONNED_DOOR = NULL;
 
 door *door_create(entity_t *id, enum DoorTransitionSpeed speed,
                   enum DoorFunction function, int wait_time, bool is_collidable,
-                  bool is_shootable, sector *sector) {
+                  bool is_shootable, bool is_repeatable,sector *sector) {
   door *d = malloc(sizeof(door));
   d->id = id;
   d->speed = speed;
@@ -66,6 +66,92 @@ void door_update(door *d, int DT) {
 void door_update_height(door *d, int delta_height) {
   d->delta_height = delta_height;
   d->max_height = d->sector->ceiling_height + delta_height;
+}
+
+door *create_door_from_linedef(sidedef *sd, enum DoorTypes line_type) {
+  door *d;
+  enum DoorTransitionSpeed speed;
+  bool repeatable;
+  switch (line_type) {
+    case CSC_SR_S:
+    case CSC_SR_F:
+      speed = line_type == CSC_SR_S ? SLOW : FAST;
+      d = door_create(NULL, speed, CLOSE_STAY_CLOSED, 0, false, false,true,sd->sector);
+      break;
+    case CSC_S1_S:
+    case CSC_S1_F:
+      speed = line_type == CSC_S1_S ? SLOW : FAST;
+      d = door_create(NULL, speed, CLOSE_STAY_CLOSED, 0, false, false,false,sd->sector);
+      break;
+    case CSC_WR_S:
+    case CSC_WR_F:
+      speed = line_type == CSC_WR_S ? SLOW : FAST;
+      d = door_create(NULL, speed, CLOSE_STAY_CLOSED, 0, true, false,true,sd->sector);
+      break;
+    case CSC_W1_S:
+    case CSC_W1_F:
+      speed = line_type == CSC_W1_S ? SLOW : FAST;
+      d = door_create(NULL, speed, CLOSE_STAY_CLOSED, 0, true, false,false,sd->sector);
+      break;
+    case CWO_SR_S:
+    case CWO_S1_S:
+      repeatable = line_type == CWO_SR_S;
+      d = door_create(NULL, SLOW, CLOSE_WAIT_OPEN, 30000, false, false,repeatable,sd->sector);
+      break;
+    case CWO_WR_S:
+    case CWO_W1_S:
+      repeatable = line_type == CWO_WR_S;
+      d = door_create(NULL, SLOW, CLOSE_WAIT_OPEN, 30000, true, false,repeatable,sd->sector);
+      break;
+    case OSO_P1_S:
+    case OSO_P1_F:
+    case OSO_WR_S:
+    case OSO_WR_F:
+    case OSO_W1_S:
+    case OSO_W1_F:
+      repeatable = line_type == OSO_WR_S || line_type == OSO_WR_S;
+      speed = (line_type == OSO_P1_S || line_type == OSO_WR_S || line_type == OSO_W1_S) ? SLOW : FAST;
+      d = door_create(NULL, speed, OPEN_STAY_OPEN, 0, true, false,repeatable,sd->sector);
+      break;
+    case OSO_SR_S:
+    case OSO_SR_F:
+    case OSO_S1_S:
+    case OSO_S1_F:
+      speed = (line_type == OSO_SR_S || line_type == OSO_S1_S) ? SLOW : FAST;
+      repeatable = line_type == OSO_SR_S || line_type == OSO_SR_S;
+      d = door_create(NULL, speed, OPEN_STAY_OPEN, 0, false, false,repeatable,sd->sector);
+      break;
+    case OSO_GR_S:
+      d = door_create(NULL, SLOW, OPEN_STAY_OPEN, 0, false, true,false,sd->sector);
+      break;
+    case OWC_PR_S:
+    case OWC_PR_F:
+    case OWC_WR_S:
+    case OWC_WR_F:
+    case OWC_W1_S:
+    case OWC_W1_F:
+      repeatable = !(line_type == OWC_W1_S || line_type == OWC_W1_S);
+      speed = (line_type == OWC_PR_S || line_type == OWC_WR_S || line_type == OWC_W1_S) ? SLOW : FAST;
+      d = door_create(NULL, speed, OPEN_WAIT_CLOSE, 4000, true, false,repeatable,sd->sector);
+      break;
+    case OWC_SR_S:
+    case OWC_SR_F:
+    case OWC_S1_S:
+    case OWC_S1_F:
+      speed = (line_type == OWC_SR_S || line_type == OWC_S1_S) ? SLOW : FAST;
+      repeatable = line_type == OWC_SR_S || line_type == OWC_SR_S;
+      d = door_create(NULL, speed, OPEN_WAIT_CLOSE, 4000, true, false,repeatable,sd->sector);
+      break;
+    case DR_RED_KEY:
+    case DR_YELLOW_KEY:
+    case DR_BLUE_KEY:
+      d = door_create(NULL, SLOW, OPEN_WAIT_CLOSE, 4000, true, true,false,sd->sector);
+      break;
+    default:
+      printf("Linedef with unknown type:  %d\n", line_type);
+      exit(1);
+  }
+  return d;
 }
 
 void door_print(door *d) {
