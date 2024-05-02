f7c4cfb438a56db16db4d0e792494df9d2ff0307
Esteban MANDJUKOWSKI <estebanmandjukowski@gmail.com>
2024-05-02 20:08:28 +0200

    feat: vssprite (yet another struct) structs and basic operations

---
+++ include/vssprite.h
@@ -0,0 +1,28 @@
+#ifndef VSSPRITE_H
+#define VSSPRITE_H
+
+#include "patch.h"
+#include "vec2.h"
+#include "geometry.h"
+#include <SDL2/SDL_stdinc.h>
+
+#define MAX_SPRITES 128
+
+struct VSSprite {
+    vec2 pos; // pos of the sprite in real world coordinates
+    int x1; // projection onto screen (left side)
+    int x2; // projection onto screen (right side)
+    double scale; // rendering scale
+    patch* sprite; 
+    Uint32 color;
+};
+
+typedef struct VSSprite vs_sprite;
+
+extern vs_sprite VSSPRITES[MAX_SPRITES];
+extern int VSSPRITES_INDEX;
+
+void vssprite_add(vec2 camera_pos, double angle, vec2 pos, patch *sprite,Uint32 color);
+
+void vssprite_sort();
+#endif
