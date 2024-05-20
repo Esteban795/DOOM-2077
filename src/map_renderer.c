9654c9a1c60249f437edd136fe1da34ffc408196
Esteban MANDJUKOWSKI <estebanmandjukowski@gmail.com>
2024-05-20 16:28:36 +0200

    chore : better memory management using static arrays :D

--- src/map_renderer.c
+++ src/map_renderer.c
@@ -1,5 +1,9 @@
 #include "../include/map_renderer.h"
 
+int ranges[WIDTH] = {0};
+double upper_clip[WIDTH] = {0};
+double lower_clip[WIDTH] = {0};
+
 double get_xy_floor_height(bsp *b, vec2 pos) {
   size_t node_id = b->root_node_id;
   while (node_id < SUBSECTOR_IDENTIFIER) {
@@ -91,44 +95,46 @@ void draw_crosshair(engine *e, color c, int size) {
                      middle_y);
 }
 
-void draw_sprite_column_full(engine *engine, patch *sprite,
-                        int sprite_column,
-                        int screen_x, int y1, int y2, double inverted_scale) {
-    double sprite_y = y1 < 0 ? -y1 * inverted_scale : 0;
-    y1 = max(y1, 0); // clipping
-    y2 = min(y2, HEIGHT - 1);
-    int sprite_y_int;
-    for (int top = y1; top < y2 - 1; top++) {
-      sprite_y_int = (int)sprite_y;
-      Uint32 pixel =
-          sprite->pixels[sprite_y_int * sprite->header.width + sprite_column];
-      if (pixel == 0) {
-        sprite_y += inverted_scale;
-        continue;
-      }
-      engine->pixels[top * WIDTH + screen_x] = pixel;
+void draw_sprite_column_full(engine *engine, patch *sprite, int sprite_column,
+                             int screen_x, int y1, int y2,
+                             double inverted_scale) {
+  double sprite_y = y1 < 0 ? -y1 * inverted_scale : 0;
+  y1 = max(y1, 0); // clipping
+  y2 = min(y2, HEIGHT - 1);
+  int sprite_y_int;
+  for (int top = y1; top < y2 - 1; top++) {
+    sprite_y_int = (int)sprite_y;
+    Uint32 pixel =
+        sprite->pixels[sprite_y_int * sprite->header.width + sprite_column];
+    if (pixel == 0) {
       sprite_y += inverted_scale;
+      continue;
     }
+    engine->pixels[top * WIDTH + screen_x] = pixel;
+    sprite_y += inverted_scale;
+  }
 }
 
 void draw_sprite_column_partial(engine *engine, patch *sprite,
-                        int sprite_column,
-                        int screen_x, int y1, int y2,double* upper_clip,double* lower_clip, double inverted_scale) {
-    int top_clip = (int)upper_clip[screen_x];
-    int bot_clip = (int)lower_clip[screen_x];
-    double sprite_y = y1 < top_clip ? -(y1 - top_clip) * inverted_scale : 0;
-    int sprite_y_int;
-    for (int top = max(top_clip,max(0,y1)); top < min(bot_clip,min(y2,HEIGHT)); top++) {
-      sprite_y_int = (int)sprite_y;
-      Uint32 pixel =
-          sprite->pixels[sprite_y_int * sprite->header.width + sprite_column];
-      if (pixel == 0) {
-        sprite_y += inverted_scale;
-        continue;
-      }
-      engine->pixels[top * WIDTH + screen_x] = pixel;
+                                int sprite_column, int screen_x, int y1, int y2,
+                                double *upper_clip, double *lower_clip,
+                                double inverted_scale) {
+  int top_clip = (int)upper_clip[screen_x];
+  int bot_clip = (int)lower_clip[screen_x];
+  double sprite_y = y1 < top_clip ? -(y1 - top_clip) * inverted_scale : 0;
+  int sprite_y_int;
+  for (int top = max(top_clip, max(0, y1));
+       top < min(bot_clip, min(y2, HEIGHT)); top++) {
+    sprite_y_int = (int)sprite_y;
+    Uint32 pixel =
+        sprite->pixels[sprite_y_int * sprite->header.width + sprite_column];
+    if (pixel == 0) {
       sprite_y += inverted_scale;
+      continue;
     }
+    engine->pixels[top * WIDTH + screen_x] = pixel;
+    sprite_y += inverted_scale;
+  }
 }
 
 double get_height_difference(bsp *b, vec2 pos1, vec2 pos2) {
@@ -169,54 +175,48 @@ void render_vssprite(engine *e, vs_sprite vssprite) {
          screen_x++) {
       sprite_column_int = (int)sprite_column;
       draw_sprite_column_full(e, sprite, sprite_column_int, screen_x, top,
-                         top + height, inverted_scale);
+                              top + height, inverted_scale);
       sprite_column += inverted_scale;
     }
   } else { // something is obscuring the sprite
-    int* ranges = malloc(sizeof(int) * WIDTH);
     memset(ranges, 0, sizeof(int) * WIDTH);
     drawseg ds;
     int marker = 0;
-
-    double* upper_clip = malloc(sizeof(double) * WIDTH);
-    double* lower_clip = malloc(sizeof(double) * WIDTH);
-    for (int i = 0; i < WIDTH; i++){
+    for (int i = 0; i < WIDTH; i++) {
       upper_clip[i] = top;
       lower_clip[i] = top + height;
     }
 
-    for (int i = 0;i < DRAWSEGS_INDEX; i++){ // calculate where exactly we can draw the sprite
+    for (int i = 0; i < DRAWSEGS_INDEX;
+         i++) { // calculate where exactly we can draw the sprite
       ds = DRAWSEGS[i];
       marker = ds.type == DRAWSEG_SOLID_WALL ? 1 : -1;
-      if (vssprite.scale < ds.scale1 && vssprite.scale < ds.scale2 && do_segs_intersect(vssprite.x1, vssprite.x2, ds.x1, ds.x2)){
-        for (int j = ds.x1 ; j < ds.x2; j++){
-            ranges[j] = marker;
-            if (marker == -1) {
-              upper_clip[j] = max(ds.top_clips[j - ds.x1],upper_clip[j]);
-              lower_clip[j] = min(ds.bottom_clips[j - ds.x1],lower_clip[j]);
-
+      if (vssprite.scale < ds.scale1 && vssprite.scale < ds.scale2 &&
+          do_segs_intersect(vssprite.x1, vssprite.x2, ds.x1, ds.x2)) {
+        for (int j = ds.x1; j < ds.x2; j++) {
+          ranges[j] = marker;
+          if (marker == -1) {
+            upper_clip[j] = max(ds.top_clips[j - ds.x1], upper_clip[j]);
+            lower_clip[j] = min(ds.bottom_clips[j - ds.x1], lower_clip[j]);
           }
         }
       }
     }
-    double sprite_column = vssprite.x1 < 0
-                               ? -vssprite.x1 * inverted_scale
-                               : 0; // clipping the sprite
+    double sprite_column = vssprite.x1 < 0 ? -vssprite.x1 * inverted_scale
+                                           : 0; // clipping the sprite
     int sprite_column_int = 0;
     for (int screen_x = max(vssprite.x1, 0); screen_x < min(vssprite.x2, WIDTH);
          screen_x++) {
       sprite_column_int = (int)sprite_column;
-      if (ranges[screen_x] == 1){ // solid wall, we can't draw anything
+      if (ranges[screen_x] == 1) { // solid wall, we can't draw anything
         sprite_column += inverted_scale;
         continue;
       }
       draw_sprite_column_partial(e, sprite, sprite_column_int, screen_x, top,
-                         top + height, upper_clip,lower_clip,inverted_scale);
+                                 top + height, upper_clip, lower_clip,
+                                 inverted_scale);
       sprite_column += inverted_scale;
     }
-    free(ranges);
-    free(upper_clip);
-    free(lower_clip);
   }
 }
 
@@ -224,8 +224,8 @@ void render_vssprites(engine *e) {
   for (int i = 0; i < VSSPRITES_INDEX; i++) {
     render_vssprite(e, VSSPRITES[i]);
   }
-  for (int i = 0; i < DRAWSEGS_INDEX;i++){
-    if (DRAWSEGS[i].type == DRAWSEG_PORTAL_WALL){
+  for (int i = 0; i < DRAWSEGS_INDEX; i++) {
+    if (DRAWSEGS[i].type == DRAWSEG_PORTAL_WALL) {
       free(DRAWSEGS[i].top_clips);
       free(DRAWSEGS[i].bottom_clips);
     }
