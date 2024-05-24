bdf59d6c4227460626dad8bd2cfa4f7053b78d03
FuseTim <fusetim@gmx.com>
2024-05-24 14:49:06 +0200

    feat: add WeaponFire & WeaponUpdate events

---
+++ include/event/player_fire.h
@@ -0,0 +1,30 @@
+#ifndef _LIB_DOOM_EVENT_PLAYERFIRE_H
+#define _LIB_DOOM_EVENT_PLAYERFIRE_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdint.h>
+
+#include "event.h"
+
+#define CLIENT_PLAYER_FIRE_EVENT_TAG 0x0045
+#define SERVER_PLAYER_FIRE_EVENT_TAG (CLIENT_PLAYER_FIRE_EVENT_TAG | 0x8000)
+
+// player_fire_event_t is an event that is triggered when a player fires a weapon.
+// It contains the entity_id of the player that fired the weapon, and the weapon_id of the weapon fired.
+typedef struct {
+    uint16_t tag;
+    // entity_id of the player
+    uint64_t entity_id;
+    // id of the weapon fired
+    uint8_t weapon_id;
+} player_fire_event_t;
+
+// ClientPlayerFireEvent creates a new ClientPlayerFireEvent.
+player_fire_event_t* ClientPlayerFireEvent_new(uint64_t entity_id, uint8_t weapon_id);
+
+// ServerPlayerFireEvent creates a new ServerPlayerFireEvent.
+player_fire_event_t* ServerPlayerFireEvent_new(uint64_t entity_id, uint8_t weapon_id);
+
+#endif
---
+++ include/event/player_weapon_update.h
@@ -0,0 +1,35 @@
+#ifndef _LIB_DOOM_EVENT_PLAYERWEAPONUPDATE_H
+#define _LIB_DOOM_EVENT_PLAYERWEAPONUPDATE_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdint.h>
+
+#include "event.h"
+#include "../component/weapon.h"
+
+#define CLIENT_PLAYER_WEAPON_UPDATE_EVENT_TAG 0x0046
+#define SERVER_PLAYER_WEAPON_UPDATE_EVENT_TAG (CLIENT_PLAYER_WEAPON_UPDATE_EVENT_TAG | 0x8000)
+
+// player_weapon_update_event_t is an event that is triggered when the weapons of a player are updated.
+// It contains the entity_id of the player, and the ammunitions, mags and cooldowns of the player.
+typedef struct {
+    uint16_t tag;
+    // entity_id of the player
+    uint64_t entity_id;
+    // ammunitions of the player
+    int ammunitions[WEAPONS_NUMBER];
+    // mags of the player
+    int mags[WEAPONS_NUMBER];
+    // cooldown by weapon of the player
+    int cooldowns[WEAPONS_NUMBER];
+} player_weapon_update_event_t;
+
+// ClientPlayerWeaponUpdateEvent creates a new ClientPlayerWeaponUpdateEvent.
+player_weapon_update_event_t* ClientPlayerWeaponUpdateEvent_new(uint64_t entity_id, int ammunitions[WEAPONS_NUMBER], int mags[WEAPONS_NUMBER], int cooldowns[WEAPONS_NUMBER]);
+
+// ServerPlayerWeaponUpdateEvent creates a new ServerPlayerWeaponUpdateEvent.
+player_weapon_update_event_t* ServerPlayerWeaponUpdateEvent_new(uint64_t entity_id, int ammunitions[WEAPONS_NUMBER], int mags[WEAPONS_NUMBER], int cooldowns[WEAPONS_NUMBER]);
+
+#endif
