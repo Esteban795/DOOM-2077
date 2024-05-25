778de6766807fba1fb0ddda50c4a82c216dd3746
FuseTim <fusetim@gmx.com>
2024-05-25 16:24:10 +0200

    doc: doc for fire and weapon update events
    A starting point towards the merge of PR!52.
    Note that the PR!52 will have to add the documentation necessary for the packets. I don't want to change too much file due to potential conflict.
    
    Ref: https://gitlab.telecomnancy.univ-lorraine.fr/ppii-2k24/video-game-02/-/merge_requests/52

--- include/event/player_fire.h
+++ include/event/player_fire.h
@@ -8,16 +8,20 @@
 
 #include "event.h"
 
+/// CLIENT_PLAYER_FIRE_EVENT_TAG is the tag of the client event that is triggered when a player fires a weapon.
 #define CLIENT_PLAYER_FIRE_EVENT_TAG 0x0045
+/// SERVER_PLAYER_FIRE_EVENT_TAG is the tag of the server event that is triggered when a player fires a weapon.
 #define SERVER_PLAYER_FIRE_EVENT_TAG (CLIENT_PLAYER_FIRE_EVENT_TAG | 0x8000)
 
-// player_fire_event_t is an event that is triggered when a player fires a weapon.
-// It contains the entity_id of the player that fired the weapon, and the weapon_id of the weapon fired.
+/// player_fire_event_t is an event that is triggered when a player fires a weapon.  
+/// It contains the entity_id of the player that fired the weapon, and the weapon_id of the weapon fired.
 typedef struct {
+    /// tag of the event
+    /// Should be CLIENT_PLAYER_FIRE_EVENT_TAG or SERVER_PLAYER_FIRE_EVENT_TAG
     uint16_t tag;
-    // entity_id of the player
+    /// entity_id of the player firing the weapon
     uint64_t entity_id;
-    // id of the weapon fired
+    /// id of the weapon fired
     int8_t weapon_id;
 } player_fire_event_t;
 
